24fb3f6bd80ef434ec636e30e92da2f3ea174649
David Ciocoiu <david.samir.ciocoiu@gmail.com>
2020-08-24 12:05:15 +0200

    mutable updated

--- ListAndTuples/mutable.py
+++ ListAndTuples/mutable.py
@@ -4,5 +4,11 @@ another_list = shopping_list
 print(id(shopping_list))
 print(id(another_list))
 
-# mutate the list
+# mutate the list:note that the ids remain unchanged
+shopping_list += ["cookies"]
+print(shopping_list)
+print("Added a new object to original list")
+print(id(shopping_list))
+print(id(another_list))
 
+# how many lists are there in the program? There is only One list.
