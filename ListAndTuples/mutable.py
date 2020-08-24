2eb375894977a4ef7aa6c269e55fdc938f21467c
David Ciocoiu <david.samir.ciocoiu@gmail.com>
2020-08-24 12:08:42 +0200

    mutable changed second list

--- ListAndTuples/mutable.py
+++ ListAndTuples/mutable.py
@@ -12,3 +12,7 @@ print(id(shopping_list))
 print(id(another_list))
 
 # how many lists are there in the program? There is only One list.
+# therefore if i append to the second list I will append to all of the instances bound to this list
+another_list.append("soap")
+print(shopping_list)
+print(another_list)
