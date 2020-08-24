83695625e8a8fb3ffd3a73af03c50886fa2d24f3
David Ciocoiu <david.samir.ciocoiu@gmail.com>
2020-08-24 13:53:32 +0200

    first example with printing numbers when iterating alongisde with printing each item in the list

--- ListAndTuples/buy_computer.py
+++ ListAndTuples/buy_computer.py
@@ -1,9 +1,17 @@
 # appending to a list
+available_parts = ["computer",
+                   "monitor",
+                   "keyboard",
+                   "mouse",
+                   "mouse mat",
+                   "hdmi cable",
+                   "dvd drive"
+                   ]
 current_choice = "-"
 computer_parts = []  # create an empty list
 
 while current_choice != "0":
-    if current_choice in "123456":
+    if current_choice in "1234567":
         print("Adding {}".format(current_choice))
         if current_choice == "1":
             computer_parts.append("computer")
@@ -20,13 +28,8 @@ while current_choice != "0":
 
     else:
         print("Please add options form the list below: ")
-        print("1: computer")
-        print("2: monitor")
-        print("3: keyboard")
-        print("4: mouse")
-        print("5: mouse mat")
-        print("6: hdmi cable")
-        print("0: to finish")
+        for part in available_parts:
+            print("{0}: {1}".format(available_parts.index(part) + 1, part))
 
     current_choice = input()
 print(computer_parts)
